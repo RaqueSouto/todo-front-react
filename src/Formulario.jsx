62201bee02f89ed256ecc798e8232237fe1840ad
RaqueSouto <76965176+RaqueSouto@users.noreply.github.com>
2024-06-03 19:11:18 +0200

    api lista

---
+++ src/Formulario.jsx
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+
+
+function Formulario({crearTareas}){
+
+    let [textoTemporal,setTextoTemporal] = useState("")
+
+    return (
+            <form onSubmit={ async evento => {
+                evento.preventDefault()
+
+                if(textoTemporal.trim() != ""){
+
+                    let {error} = await fetch("http://localhost:4000/tareas/nueva", {
+                        method : "POST",
+                        body : JSON.stringify({ tarea : textoTemporal.trim() }),
+                        headers : {
+                            "Content-type" : "application/json"
+                        }
+
+                }).then(respuesta => respuesta.json())
+
+                if(!error){
+                    crearTareas({
+                        id : Math.random(),
+                        tarea : textoTemporal.trim(),
+                        terminada : false
+                    })
+                    return setTextoTemporal("")
+                }
+                console.log("error a usuario")
+
+            }
+
+            } }>
+                <input type="text" 
+                placeholder="¿qué hay que hacer?" 
+                value={textoTemporal}
+                onChange={ evento => setTextoTemporal(evento.target.value) } />
+                <input type="submit" value="crear tarea"/>
+            </form>
+
+            )
+}
+
+export default Formulario
